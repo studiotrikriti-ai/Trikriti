d191f4e7e88db3b66d58573d210bd7b443c52091
Rohit Kamble <rohitkamble171012@gmail.com>
2026-04-29 10:45:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const keepAlive = require("./keepAlive");
+const keepAlive = require("./utils/keepAlive");
 
 
 const productRoutes = require("./routes/productRoutes");
