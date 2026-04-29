710442fb42b82619450c67cfed25826dba252f3f
Rohit Kamble <rohitkamble171012@gmail.com>
2026-04-29 13:04:51 +0530

    Refactor CORS configuration for allowed origins

--- server/index.js
+++ server/index.js
@@ -13,7 +13,22 @@ const customOrderRoutes = require("./routes/customOrderRoutes");
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:3000", credentials: true }));
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://trikritistudio.in",
+  "https://www.trikritistudio.in",
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
